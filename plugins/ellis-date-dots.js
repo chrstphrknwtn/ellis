4b1378ba329600308aef2baa1fc46c64c0fbbd58
Christopher Newton <christopher.k.newton@me.com>
2016-10-04 17:38:16 +0200

    Fading date dots plugin

---
+++ plugins/ellis-date-dots.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/* minimal fading dots for file mtime
+------------------------------------------------*/
+module.exports = function rottingDatesPlugin(stats) {
+  const now = Date.now()
+
+  const column = []
+
+  stats.forEach(file => {
+    const fileAge = now - file.stat.mtime
+
+    const colorMin = 20
+
+    const ageRange = 86400000 * 10 // one day * 10
+    const ageMultiplier = Math.max(0, (ageRange - fileAge) / ageRange)
+    const ageAddon = ((255 - colorMin) * ageMultiplier) + colorMin
+    let ageColor = parseInt(ageAddon, 10)
+
+    const colorCode = `\x1b[38;2;${ageColor};${ageColor};${ageColor}m`
+
+    column.push(` ${colorCode}·`)
+  })
+
+  return column
+}
